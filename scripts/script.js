502a4a649d30b7c32742c09ebf9938485f2f4bd4
jared <jared.jacobson.2000@gmail.com>
2017-10-26 20:50:56 -0600

    created the style.scss file

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,27 @@
-class list {
-    constructor(name) {
+let allLists = {
+    listContainer: [],
+    listNameArray: [],
+    addList: function (listName) {
+        this.listNameArray.push(listName);
+        let divIndex = this.listNameArray.length;
+        this.listContainer.push(new List(listName, divIndex));
+        document.getElementById('todoListBarCont').innerHTML +=
+            '<div id="list' + divIndex + '">' + listName + '</div>';
+    },
+    removeList: function (listName) {
+        delete this.listContainer[listName];
+    }
+};
+class List {
+    constructor(name, listDivIndex) {
         this.name = name;
         this.tasks = [];
         this.completedTasks = [];
-    }
-    addList() {
-
-    }
-    removeList() {
-
+        this.domElemement = document.getElementById('list'+ listDivIndex);
     }
     changeListName(newName) {
         this.name = newName;
+        this.domElemement.innerHTML = newName;
     }
     addItem(listItem) {
         this.tasks[listItem.index] += listItem;
@@ -30,5 +40,14 @@ class listItem {
         this.date = date;
         this.time = time;
     }
-
+    
+}
+function displayAllListArray() {
+    allLists.listNameArray.forEach(function (value, index) {
+        document.getElementById('toDoList').innerHTML +=
+            '<div class="listNames" id="list' + index + '">' + value + '</div>';
+    });
+}
+function createNewList(listName) {
+    allLists.addList(listName);
 }
