0e1c98e0109d69aeced00476863b73404faa1498
jared <jared.jacobson.2000@gmail.com>
2017-11-01 09:14:55 -0600

    I haven't commited for a while so I have done a lot.

---
+++ scripts/domElements.js
@@ -0,0 +1,3 @@
+let DomElements = {
+    "": 0
+};
--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,25 @@
 let allLists = {
-    listContainer: [],
+    listContainer: {},
     listNameArray: [],
     addList: function (listName) {
-        this.listNameArray.push(listName);
-        let divIndex = this.listNameArray.length;
-        this.listContainer.push(new List(listName, divIndex));
-        document.getElementById('todoListBarCont').innerHTML +=
-            '<div id="list' + divIndex + '">' + listName + '</div>';
+        allLists.listNameArray.push(listName);
+        let index = allLists.listNameArray.length;
+        allLists.listContainer["list" + index] = new List(listName, index);
+        document.getElementById('todoListDrawerListCont').innerHTML +=
+            '<a id="list' + index + '" class="mdl-navigation__link" onclick="displayList()">' + listName + '</a>';
     },
-    removeList: function (listName) {
-        delete this.listContainer[listName];
+    removeList: function (listIndex) {
+        document.getElementById(listIndex);
+        delete allLists.listContainer[listIndex];
     }
 };
 class List {
-    constructor(name, listDivIndex) {
+    constructor(name, listIndex) {
         this.name = name;
         this.tasks = [];
+        this.listIndex = listIndex;
         this.completedTasks = [];
-        this.domElemement = document.getElementById('list'+ listDivIndex);
+        this.domElemement = document.getElementById('list'+ listIndex);
     }
     changeListName(newName) {
         this.name = newName;
@@ -26,11 +28,17 @@ class List {
     addItem(listItem) {
         this.tasks[listItem.index] += listItem;
     }
-    completeItem() {
-        this.completedTasks += listItem;
+    completeItem(listItem) {
+        this.completedTasks.push(listItem);
+    }
+    displayListTools(listIndex) {
+        
+    }
+    displayList(listName) {
+
     }
 }
-class listItem {
+class ListItem {
     constructor(task, list, index) {
         this.task = task;
         this.list = list;
@@ -40,7 +48,6 @@ class listItem {
         this.date = date;
         this.time = time;
     }
-    
 }
 function displayAllListArray() {
     allLists.listNameArray.forEach(function (value, index) {
@@ -50,4 +57,23 @@ function displayAllListArray() {
 }
 function createNewList(listName) {
     allLists.addList(listName);
+}
+function displayList(listName) {
+    document.getElementById('mainContent').innerHTML +=
+        "<div class='mdl-card listDisplay mdl-shadow--2dp'>" +
+            "<div class='mdl-card__title'>" +
+                "<h1 class='mdl-card__title'>" + listName + "</h1>" +
+            "</div>" +
+            "<div class='taskContainer' id='" + listName + "taskContainer'>" +
+
+            "</div>" +
+        "</div>";
+    allLists.listContainer[listName].tasks.forEach(function (value, index, array) {
+
+    })
+}
+function displayTask(task, listIndex) {
+    let container = document.getElementById(listName + 'taskContainer');
+    container.innerHTML +=
+        "<div>";
 }
